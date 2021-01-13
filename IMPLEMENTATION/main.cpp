dce081fc7871a2c68c1d1239da65097aab11e010
MCLEANS <jackagwaya@gmail.com>
2021-01-13 16:27:13 +0300

    deleted default content

--- IMPLEMENTATION/main.cpp
+++ IMPLEMENTATION/main.cpp
@@ -93,20 +93,9 @@ int main(void) {
    * Set initial duty cycle
    */
   servo.set_duty_cycle(starting_angle);
-  previous_angle = starting_angle;
+  previous_angle = starting_angle; 
   
-  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-  
-  GPIOA->MODER |= GPIO_MODER_MODER7_0;
-  GPIOA->MODER |= GPIO_MODER_MODER6_0;
-
-  GPIOA->ODR |= GPIO_ODR_ODR_6;
-  GPIOA->ODR &= ~GPIO_ODR_ODR_7;  
-
   while(1){
-    for(volatile int i = 0; i < 2000000; i++){}
-    GPIOA->ODR ^= (1<<6);
-    GPIOA->ODR ^= (1<<7);
-
+  
   }
 }
