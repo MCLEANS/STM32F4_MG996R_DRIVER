fe7dd5341e0861f2584708c1a82ae0d399f2b8f6
MCLEANS <jackagwaya@gmail.com>
2021-01-14 08:50:26 +0300

    Added STM32 header"

--- IMPLEMENTATION/MG996R.h
+++ IMPLEMENTATION/MG996R.h
@@ -1,12 +1,18 @@
 #ifndef _MG996R_H
 #define _MG996R_H
 
+#include "stm32f4xx.h"
+
 namespace custom_libraries{
 class MG996R{
     private:
+        int starting_angle = 90;
+        int previous_angle = 0;
     private:
     public:
     public:
+        MG996R();
+
 
 };
 
