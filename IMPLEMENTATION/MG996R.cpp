37adc3271af7221e12f6a2dfeb4dc7e04c0c2b3b
MCLEANS <jackagwaya@gmail.com>
2021-01-14 09:05:06 +0300

    defined servo constructor and destructor

--- IMPLEMENTATION/MG996R.cpp
+++ IMPLEMENTATION/MG996R.cpp
@@ -1,6 +1,26 @@
 #include "MG996R.h"
 
 namespace custom_libraries{
-    
+
+MG996R::MG996R(TIM_TypeDef *TIMER,
+                channel input_channel,
+                GPIO_TypeDef *PORT,
+                uint8_t PIN,
+                alternate_function pin_function,
+                uint16_t prescaler,
+                uint16_t auto_reload_value):PWM(TIMER,
+                                                input_channel,
+                                                PORT,
+                                                PIN,
+                                                pin_function,
+                                                prescaler,
+                                                auto_reload_value){
+
+ }
+
+ MG996R::~MG996R(){
+     
+ }
+
 }
 
